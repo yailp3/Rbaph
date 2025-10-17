3ad994fff16bbcbd125d5029325fcca33744ca55
yailp3 <yacine.abdelsadok@imt-atlantique.net>
2025-10-17 14:33:41 +0200

    comment controler manettes

--- Rbaph_controler/Rbaph_parameters.m
+++ Rbaph_controler/Rbaph_parameters.m
@@ -1,35 +1,37 @@
-
 clear
 
 %%
 
-m = 2.040; % [kg]
-I = 0.03; % [kg.m^2]
-c_t = [1.4*10^-6; 1.4*10^-6; 1.4*10^-6; 1.4*10^-6]; % kg.m.rad-2  %%% warning, was previously a constant %%%
+m = 2.040; % Mase du robot en [kg]
+I = 0.03; % Moment d'inertie [kg.m^2]
+M = diag([m, m, I]);
+c_t = [1.4*10^-6; 1.4*10^-6; 1.4*10^-6; 1.4*10^-6]; % trust coefficient des hélices en [kg.m.rad-2]  %%% warning, was previously a constant %%%
+
+
+G = [0;0]; % si le centre de masse est mal placé, modifier cette variable.
+
+% position des hélices
 theta1 = -pi/4;
 theta2 = -theta1;
 theta3 = pi;
 theta4 = theta3;
-
-G = [0;0];
-
-% position des hélices
 r1 = [G(1)-0.125;G(2)+0.060;0];
 r2 = [G(1)-0.125;G(2)-0.060;0];
 r3 = [G(1)+0.080;G(2)+0.070;0];
 r4 = [G(1)+0.080;G(2)-0.070;0];
 
+% Gains
 Kpx=0;
 Kpy=3;
 Kptheta=0;
 Kp=diag([Kpx, Kpy, Kptheta]);
 Kd = Kp*0.1;
 
-f_min = 0.0615 * ones(4,1); % 2 001 rpm
-%f_min = 1.8577 * ones(4,1); % 11 000 rpm
-f_max = 5 * ones(4,1);% 18 500 rpm
-%f_max = 6.1411 * ones(4,1); 20 000 rpm
+% Plage de force des moteurs
+f_min = 0.0615 * ones(4,1); % 2 001 rpm, minimum de betalink
+f_max = 5 * ones(4,1);% 18 500 rpm, maximum du plus faible des moteurs
 
+% Matrice d'allocation
 f1 = [cos(theta1); sin(theta1); 0];
 f2 = [cos(theta2); sin(theta2); 0];
 f3 = [cos(theta3); sin(theta3); 0];
@@ -41,7 +43,6 @@ W = [
     cross(r1, f1)' * [0; 0; 1], cross(r2, f2)' * [0; 0; 1], cross(r3, f3)' * [0; 0; 1], cross(r4, f4)' * [0; 0; 1]
     ];
 
-f0 = null(W); % calculer annalitiquement le null space
+% null space
+f0 = null(W); 
 f0 = f0/min(f0);
-
-M = diag([m, m, I]);
